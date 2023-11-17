0fb135ed9c6fd2d6c951977383a5a3449d124d81
lee_c <gitleelee@gmail.com>
2023-11-16 17:34:00 -0800

    Added properties, a celebrate method, and run method.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const body = document.querySelector("body");
 const success = document.querySelector(".success");
 const dance = document.querySelector(".dance");
 
+// properties
 const learning = {
   topic: "JS",
   learningGoals: [
@@ -14,11 +15,29 @@ const learning = {
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
-  weeklyHourGoal: 7
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function (hours) {
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    }
+  }, 
+  // celebrate method
+  celebrate: function() {
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  }
 };
+console.log(learning);
 
+// call method
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
-countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
+  countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
-topicElement.classList.remove("hide");
-countElement.classList.remove("hide");
+  topicElement.classList.remove("hide");
+  countElement.classList.remove("hide");
+
+  learning.addStudyTime(5);
